6d2441496606c5a2f2d75638159209d7c1125be0
Alexandr Korsak <alex.korsak@gmail.com>
2012-06-28 14:13:22 +0300

    Initial commit with basic gem structure

---
+++ lib/deployments-app.rb
@@ -0,0 +1,7 @@
+require "deployments-app/version"
+
+module Deployments
+  module App
+    # Your code goes here...
+  end
+end
---
+++ lib/deployments-app/version.rb
@@ -0,0 +1,5 @@
+module Deployments
+  module App
+    VERSION = "0.0.1"
+  end
+end
