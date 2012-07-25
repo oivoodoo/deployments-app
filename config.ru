bc915f6243e67c7795a44db4fbbc968a64d6b6db
Alexandr Korsak <alex.korsak@gmail.com>
2012-07-25 22:48:21 +0300

    Move server configuration to the server sinatra base

--- config.ru
+++ config.ru
@@ -1,14 +1,5 @@
 require 'deployments-app'
 require 'sinatra'
 
-class Deployments::App::Server < Sinatra::Base
-   set :public_folder, File.dirname(__FILE__) + "/public"
-
-  configure do
-    DataMapper.setup(:default, ENV["DATABASE_URL"] || "mysql://root@localhost/deployments-app-development")
-    DataMapper.auto_upgrade!
-  end
-end
-
 run Deployments::App::Server
 
--- lib/deployments-app/server.rb
+++ lib/deployments-app/server.rb
@@ -4,6 +4,17 @@ module Deployments
       use Routes::Authentication
       use Routes::Root
       use Routes::Projects
+
+      set :public_folder, File.expand_path(File.dirname(__FILE__) + "/../../public")
+
+      def self.development_db_path
+        "mysql://root@localhost/deployments-app-development"
+      end
+
+      configure do
+        DataMapper.setup(:default, ENV["DATABASE_URL"] || self.development_db_path)
+        DataMapper.auto_upgrade!
+      end
     end
 
     Sinatra::Base.send(:include, Routes::Extensions)
